4ebabcc129826c1e4ab85fb6a215bb25b5109987
Wojciech Ciezobka <wciezobka@student.agh.edu.pl>
2024-04-10 11:29:48 +0200

    Added explore split plotting

--- utils/explore_split.py
+++ utils/explore_split.py
@@ -1,5 +1,4 @@
 import os
-from tqdm import tqdm
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -52,70 +51,19 @@ def explore_files(directory_path: str, min_length: int = 200, plot: bool = True)
     
     return lengths_df
 
-
-def split_file_equally(file: pd.Series, source_dir_path: str, destination_dir_path: str, min_length: int = 200):
-
-    def split_file(file: pd.Series, start: int, end: int, suffix: str):
-        # Read the file
-        df = pd.read_csv(os.path.join(source_dir_path, file["FileName"]), sep='\t', header=None)
-
-        # Cut the interesting part
-        df = df.iloc[start:end+1]
-
-        # Save the file with the new name in the destination directory
-        df.to_csv(
-            os.path.join(destination_dir_path, file["SubjectName"] + suffix + ".tsv"),
-            sep='\t', index=False, header=False
-        )
-    
-    length = file["Length"]
-    suffix_id = 65
-    if length < 2 * min_length:
-        split_file(file, 0, length - 1, chr(suffix_id))
-        suffix_id += 1
-    else:
-        start = 0
-        end = min(length, min_length) - 1
-        split_file(file, start, end, chr(suffix_id))
-        suffix_id += 1
-        while end + 2 * min_length < length:
-            start += min_length
-            end += min_length
-            split_file(file, start, end, chr(suffix_id))
-            suffix_id += 1
-        start += min_length
-        end = length - 1
-        split_file(file, start, end, chr(suffix_id))
-        suffix_id += 1
-
-
 if __name__ == "__main__":
 
     # Program arguments
     parser = ArgumentParser()
-    parser.add_argument('-s', '--source_dir', type=str, help="Source directory where long files are stored")
-    parser.add_argument('-d', '--destination_dir', type=str, help="Destination directory where short files will be stored")
-    parser.add_argument('-l', '--min_length', type=int, help="The minimum length of the splitted timeseries")
-    parser.add_argument('-p', '--plot', default=False, action='store_true', 
+    parser.add_argument('-s', '--source_dir', type=str, required=True, help="Source directory where timeseries files are stored")
+    parser.add_argument('-l', '--min_length', type=int, default=200, help="The minimum length of the splitted timeseries")
+    parser.add_argument('-p', '--plot', default=True, action='store_false', 
                         help="Should the prior distribution of lengths be plotted?")
     args = parser.parse_args()
 
     source_dir = args.source_dir
-    destination_dir = args.destination_dir
     min_length = args.min_length
     plot = args.plot
 
     # Explore initial files
-    lengths_df = explore_files(source_dir, min_length=min_length, plot=plot)
-
-    # Split files
-    # - Create destination directory
-    assert not os.path.exists(destination_dir), "Destination directory already exists! Remove it before running this script."
-    os.system(f"mkdir {destination_dir}")
-
-    # - Split files
-    for index, file in tqdm(lengths_df.iterrows()):
-        split_file_equally(file, source_dir, destination_dir, min_length=min_length)
-    
-    # - Explore files
-    explore_files(destination_dir, min_length=min_length, plot=plot)
+    explore_files(source_dir, min_length=min_length, plot=plot)
